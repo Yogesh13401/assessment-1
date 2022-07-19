afd221d8f66836e0c59ab72149c5e0a31c11a9a8
Yogesh S <yogesh.s@prodapt.com>
2022-07-19 13:02:41 +0530

    Source code

---
+++ TesterSI.java
@@ -0,0 +1,14 @@
+package com.SIpackage;
+
+public class TesterSI {
+	public static void main(String[] args) { 
+
+		// TODO Auto-generated method stub 
+
+		SimpleInterest k = new SimpleInterest(); 
+
+		k.SimpleInterest(30000,12,3); 
+
+		} 
+
+}
