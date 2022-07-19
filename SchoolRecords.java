d08571227a2aff6c4ec6d54ac55fbf1708bc273b
Yogesh S <yogesh.s@prodapt.com>
2022-07-19 12:57:00 +0530

    Function Code

---
+++ SchoolRecords.java
@@ -0,0 +1,18 @@
+package com.schoolrecords;
+
+public class SchoolRecords {
+	void ddRecords() 
+    { 
+      System.out.println("The Records are saved"); 
+    } 
+    void dispRecords(int x,String y,String z) 
+    { 
+      System.out.println("SchoolId.. " + x ); 
+
+	  System.out.println("SchoolName.. " + y); 
+
+	  System.out.println("SchoolAddress.. " + z); 
+
+	}
+
+}
